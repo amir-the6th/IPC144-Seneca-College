1aca8edfcde594e950ee316503e2dec89e8ef8c4
Amir Sabagh <69528025+amir-the6th@users.noreply.github.com>
2020-12-14 20:30:16 -0500

    Add files via upload

---
+++ Workshop2/in_lab/cashRegister - Copy.c
@@ -0,0 +1,39 @@
+//==============================================
+// Name:           Amirhossein Sabagh
+// Student Number: 152956199
+// Email:          asabagh@myseneca.ca
+// Section:        NFF
+// Workshop:       2 (in-lab)
+//==============================================
+// Start your code below:
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) {
+	double amount = 0.0 ;
+	int cents, loonies;
+	float owe;
+	printf("Please enter the amount to be paid : $");
+		scanf("%lf", &amount);
+	
+	cents = amount * 100;
+	amount += 0.5;
+	
+	loonies = cents / 100;
+	
+	cents = cents % 100;
+	
+	owe = cents / 100.00;
+	printf("Loonies required: %d, balance owing $%.2f\n", loonies, owe);
+
+	loonies = cents / 25.00;
+	cents = cents % 25;
+
+	printf("Quarters required: %d, balance owing $%.2f\n", loonies, owe);
+
+		return 0;
+}
+
+// dime = 10 cents
+// nickle = 5 cents
+// pennie = 1 cent
